7235087916114a0fd0f108d71ae87e6027e376ed
yiwen.zhao <yiwen.zhao@bytedance.com>
2025-12-02 23:48:44 +0000

    initialize

--- veomni/models/transformers/qwen3_omni/__init__.py
+++ veomni/models/transformers/qwen3_omni/__init__.py
@@ -5,18 +5,29 @@ from transformers import (
     AutoProcessor,
 )
 
-from transformers.utils import logging
-from ..utils.import_utils import is_transformers_version_greater_or_equal_to  # 路径按实际工程调整
-
+from ....utils.import_utils import is_transformers_version_greater_or_equal_to
 from .configuration_qwen3_omni import Qwen3OmniConfig
 from .modeling_qwen3_omni import Qwen3OmniForConditionalGeneration
 from .processing_qwen3_omni import Qwen3OmniProcessor
 
-logger = logging.get_logger(__name__)
 
-# TODO: 确认从哪个 transformers 版本开始官方支持 Qwen3 Omni，如果没有官方支持，这个判断可以先去掉
-if not is_transformers_version_greater_or_equal_to("4.99.0"):
+# After 4.57, this model is already registered in transformers. Registering again
+# will cause "already exists" errors.
+if not is_transformers_version_greater_or_equal_to("4.57.0"):
     AutoConfig.register("qwen3_omni", Qwen3OmniConfig)
     AutoModel.register(Qwen3OmniConfig, Qwen3OmniForConditionalGeneration)
     AutoModelForCausalLM.register(Qwen3OmniConfig, Qwen3OmniForConditionalGeneration)
     AutoProcessor.register(Qwen3OmniConfig, Qwen3OmniProcessor)
+
+
+
+
+
+
+# After 4.52, this model is already registered in transfomers. Register will cause
+# already exists error.
+if not is_transformers_version_greater_or_equal_to("4.52.0"):
+    AutoConfig.register("qwen2_5_omni", Qwen2_5OmniConfig)
+    AutoModel.register(Qwen2_5OmniConfig, Qwen2_5OmniForConditionalGeneration)
+    AutoModelForCausalLM.register(Qwen2_5OmniConfig, Qwen2_5OmniForConditionalGeneration)
+    AutoProcessor.register(Qwen2_5OmniConfig, Qwen2_5OmniProcessor)
